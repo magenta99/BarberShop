1e29aa72b2066d002d149b5fb7264f127fef08a1
Huy Anh <lehuyanhtvo1@gmail.com>
2019-10-11 22:56:49 +0700

    'first'

--- controllers/scheduleController.js
+++ controllers/scheduleController.js
@@ -3,8 +3,9 @@ var router = express.Router();
 const mongoose = require('mongoose');
 const Product = mongoose.model('Product')
 
-router.get("/result",(req,res)=>{
-    Product.find().exec((err,docs)=>{
+router.get("/result/:id",(req,res)=>{
+    var id = req.params.id;
+    Product.find({typeProduct:id}).exec((err,docs)=>{
         res.send(docs)
     })
 })
