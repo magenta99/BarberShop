802ff46d1f9639b2622d3444ce0dd470ce172209
Huy Anh <lehuyanhtvo1@gmail.com>
2019-10-23 01:11:53 +0700

    First commit

--- controllers/scheduleController.js
+++ controllers/scheduleController.js
@@ -5,23 +5,38 @@ const Product = mongoose.model('Product')
 const Order = mongoose.model('Order')
 var express = require('express')
 var bodyParser = require('body-parser')
- 
+
 var app = express()
- 
+
 // create application/json parser
 var jsonParser = bodyParser.json()
- 
+
 // create application/x-www-form-urlencoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-router.post('/order',urlencodedParser,(req,res)=>{
-    var body = req.body;
-    res.send(body);
+router.post('/order', urlencodedParser, (req, res) => {
+    var imageProduct = req.body.imageProduct;
+    var nameProduct = req.body.nameProduct;
+    var priceProduct = req.body.priceProduct;
+    var fullName = req.body.fullName;
+    var phoneNumber = req.body.phoneNumber;
+    var address = req.body.address;
+
+    Order.create([
+    {
+        "imageProduct":imageProduct,
+        "nameProduct":nameProduct,
+        "priceProduct":priceProduct,
+        "fullName":fullName,
+        "phoneNumber":phoneNumber,
+        "address":address
+    }
+])
 })
 
-router.get("/result/:id",(req,res)=>{
+router.get("/result/:id", (req, res) => {
     var id = req.params.id;
-    Product.find({typeProduct:id}).exec((err,docs)=>{
+    Product.find({ typeProduct: id }).exec((err, docs) => {
         res.send(docs)
     })
 })
